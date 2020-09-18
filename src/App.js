af5aa0138f0cb82e13545d15fa785c4f9d2331ae
chlwogur1228 <chlwogur1228@gmail.com>
2020-09-18 12:13:14 +0900

    0918 학습 및  Readme 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+//import Potato from './Potato';
 
+function Food({fav}) {
+  
+  return (
+    <h3>I Love {fav}</h3>
+  )
+}
 
 function App() {
   //return <div className="App" />;
- return <div>
+ return( 
+ <div>
+
    <h1>hello</h1>
-   </div>;
+  <Food fav="김치"/>
+  <Food fav="김치만두"/>
+  <Food fav="김치찌개"/>
+  <Food fav="김치볶음밥"/>
+  <Food fav="김치전"/>
+   </div>
+   );
 }
 
 export default App;
