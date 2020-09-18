72a5745af675e598caca5574fdc93d8b957ce2eb
chlwogur1228 <chlwogur1228@gmail.com>
2020-09-18 09:52:22 +0900

    0911

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## Movie_App_2020
 
 React Js Fundamentals Course 2020
+
+## 0911
+>1. Dothome index파일 수정
+>2. Readme 수정 후 Github에 Push
--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 
 function App() {
-  return <div className="App" />;
-  
+  //return <div className="App" />;
+ return <div>
+   <h1>hello</h1>
+   </div>;
 }
 
 export default App;
